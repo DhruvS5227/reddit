982b08b65dedaa506bc9ad2bdfd42fa092cffc83
DhruvS5227 <80899325+DhruvS5227@users.noreply.github.com>
2021-07-29 20:15:20 +0530

    Update reddit_posts.py

--- reddit_posts.py
+++ reddit_posts.py
@@ -18,26 +18,26 @@ api = PushshiftAPI()
 # In[3]:
 
 
-start_epoch=int(dt(2019, 1, 1).timestamp()) 
-end_epoch=int(dt(2021, 7, 8).timestamp())
+start_epoch=int(dt(2019, 3, 30).timestamp()) #start date 
+end_epoch=int(dt(2021, 7, 30).timestamp()) # end date
 
 while start_epoch < end_epoch + 86200:    
-    api_request_generator =  api.search_submissions(subreddit='4kTV', after = start_epoch, before = start_epoch + 95000000) 
+    api_request_generator =  api.search_submissions(subreddit='#subreddit_name', after = start_epoch, before = start_epoch + 95000000)   # 86200 * duration of collection(number of days)   
  
- # 86200 * duration of collection 
+ 
     tv_submissions = pd.DataFrame([submission.d_ for submission in api_request_generator])
     tv_submissions['date'] = pd.to_datetime(tv_submissions['created_utc'], utc=True, unit='s')
     tv_central = tv_submissions[['title', 'score', 'num_comments', 'selftext', 'link_flair_text','date']]
-    
+    # title, number of upvotes, number of comments, body, discussion type, date 
     start_date = dt.fromtimestamp(start_epoch)
     dateStr = start_date.strftime("%Y %b %d")
     
-    tv_central.to_csv(r"C:\\Users\\DELL\\Desktop\\hello.csv", index = False, header = True)
+    tv_central.to_csv(r"#folder_location .csv", index = False, header = True)
     
-    start_epoch += 95000000 # 86000 * duration of collection
+    start_epoch += 95000000 # 86000 * duration of collection(number of days)
 
 
-# In[ ]:
+
 
 
 
